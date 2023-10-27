6b0d80c817a0b4c09a8066008c50fd86fa8f0d54
santiago abastante <santiago.abastante@gmail.com>
2023-10-27 15:06:21 -0300

    upload terraform file for ec2 instances creation

---
+++ santi_test/ec2_vpn.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ubuntu-santi-server" {
+  ami                         = "ami-0af6e9042ea5a4e3e"
+  instance_type               = "t2.small"
+  key_name                    = aws_key_pair.santi-key.key_name
+  subnet_id                   = "subnet-04cdf8956d97d2f1b"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["sg-06d84080086d6da69"]
+  disable_api_termination = true
+}
